130e9036486e21796bd80e908bf7bb1ee58b213f
Omar Morales <moralesacevedoomaralejandro.et@gmail.com>
2025-10-02 23:18:22 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,24 +1,28 @@
+
 import type { ReactNode } from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import Image from "next/image";
+import { FirebaseClientProvider } from "@/firebase";
 
 export function PublicLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="w-full bg-white flex justify-center py-2 border-b">
-         <Image 
-          src="/header-logos.png"
-          alt="Logos de la UAT y Facultad de Ingeniería de Tampico"
-          width={400}
-          height={60}
-          className="object-contain"
-          priority
-        />
-      </header>
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
+    <FirebaseClientProvider>
+      <div className="flex flex-col min-h-screen">
+        <header className="w-full bg-white flex justify-center py-2 border-b">
+          <Image
+            src="/header-logos.png"
+            alt="Logos de la UAT y Facultad de Ingeniería de Tampico"
+            width={400}
+            height={60}
+            className="object-contain"
+            priority
+          />
+        </header>
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </div>
+    </FirebaseClientProvider>
   );
 }
