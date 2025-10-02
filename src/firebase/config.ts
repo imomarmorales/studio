3db682ceced01d9c6f0da9abc6d18d94dec11d6c
Omar Morales <moralesacevedoomaralejandro.et@gmail.com>
2025-10-02 22:41:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-496281858-a9fee",
+  "appId": "1:436370929787:web:44fcc89637e2137242cade",
+  "apiKey": "AIzaSyAg5rcGKPgP_sHK9cIfttbK9AJZJS9ndGk",
+  "authDomain": "studio-496281858-a9fee.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436370929787"
+};
